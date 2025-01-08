e0d06b3cd532b2142999aca3f65ecec34393e507
emil norsker <emilgraaenorsker@gmail.com>
2025-01-08 10:06:28 +0100

    feat: documentation in the editor

--- nobodywho/src/lib.rs
+++ nobodywho/src/lib.rs
@@ -14,28 +14,41 @@ unsafe impl ExtensionLibrary for NobodyWhoExtension {}
 
 #[derive(GodotClass)]
 #[class(tool, base=Resource)]
+/// Sampler configuration for the LLM.
+/// This tool will help you determine the behavior for you chat.
 struct NobodyWhoSampler {
     base: Base<Resource>,
 
     #[export]
+    /// The seed for the LLM. This ensures detmerminism across runs.
     seed: u32,
     #[export]
+    /// The temperature for the LLM. This controls the randomness of the LLM. A high temperature
+    /// will make the LLM "creative" with its responses, while a low score will make it more deterministic.
     temperature: f32,
     #[export]
+    /// Controls how many previous token are taken into consideration when calculating repeat penalty.
     penalty_last_n: i32,
     #[export]
+    /// Controls the penalty for repeating tokens within the last n tokens. This varies a lot between models.
     penalty_repeat: f32,
     #[export]
+    /// Decreases the likelihood of repeating tokens based on how often they appear.
     penalty_freq: f32,
     #[export]
+    /// Binary penalty if the token has apeared before.
     penalty_present: f32,
     #[export]
+    /// Penalizes newlines.
     penalize_nl: bool,
     #[export]
+    /// Ignores end of sentence tokens.
     ignore_eos: bool,
     #[export]
+    /// Controls the target perplexity for Mirostat.
     mirostat_tau: f32,
     #[export]
+    /// Controls the learning rate for Mirostat.
     mirostat_eta: f32,
 }
 
@@ -78,6 +91,9 @@ impl NobodyWhoSampler {
 
 #[derive(GodotClass)]
 #[class(base=Node)]
+/// The model node is used to load the model, currently only GGUF models are supported.
+///
+/// If you dont know what model to use, we would suggest checking out https://huggingface.co/spaces/k-mktr/gpu-poor-llm-arena
 struct NobodyWhoModel {
     #[export(file = "*.gguf")]
     model_path: GString,
@@ -129,18 +145,53 @@ impl NobodyWhoModel {
 
 #[derive(GodotClass)]
 #[class(base=Node)]
+/// NobodyWhoChat is the main node for interacting with the LLM. It functions as a chat, and can be used to send and receive messages.
+///
+/// The chat node is used to start a new context to send and receive messages (multiple contexts can be used at the same time with the same model).
+/// It requires a call to `start_worker()` before it can be used. If you do not call it, the chat will start the worker when you send the first message.
+/// OBS: The current implementation is dependant on the physics process to handle the LLM output.
+/// This is not the best way to handle this, and means that you cant overwrite the physics process
+///  on any class that inherits NobodyWhoChat.
+///
+/// Example:
+///
+/// ```
+/// extends NobodyWhoChat
+///
+/// func _ready():
+///     # configure node
+///     self.model_node = get_node("../ChatModel")
+///     self.system_prompt = "You are an evil wizard. Always try to curse anyone who talks to you."
+///
+///     # say something
+///     say("Hi there! Who are you?")
+///
+///     # wait for the response
+///     var response = await response_finished
+///     print("Got response: " + response)
+///
+///     # in this example we just use the `response_finished` signal to get the complete response
+///     # in real-world-use you definitely want to connect `response_updated`, which gives one word at a time
+///     # the whole interaction feels *much* smoother if you stream the response out word-by-word.
+/// ```
+///
 struct NobodyWhoChat {
     #[export]
+    /// The model node for the chat.
     model_node: Option<Gd<NobodyWhoModel>>,
 
     #[export]
+    /// The sampler configuration for the chat.
     sampler: Option<Gd<NobodyWhoSampler>>,
 
     #[export]
     #[var(hint = MULTILINE_TEXT)]
+    /// The system prompt for the chat, this is the basic instructions for the LLM's behavior.
     system_prompt: GString,
 
     #[export]
+    /// This is the maximum number of tokens that can be stored in the chat history. it will delete information from the chat history if it exceeds this limit.
+    /// Set this as high as needed, as it will also increase the memory usage of the LLM.
     context_length: u32,
 
     prompt_tx: Option<Sender<String>>,
@@ -211,6 +262,8 @@ impl NobodyWhoChat {
     }
 
     #[func]
+    /// Starts the LLM worker thread. This is required before you can send messages to the LLM.\
+    /// currently this is a blocking call, so be wise with when you call it.
     fn start_worker(&mut self) {
         let mut result = || -> Result<(), String> {
             let model = self.get_model()?;
@@ -256,21 +309,65 @@ impl NobodyWhoChat {
     }
 
     #[func]
+    /// Sends a message to the LLM. This will return a signal that you can use to wait for the response.
+    /// This will start the inference process. meaning you can also listen on the `response_updated` and `response_finished` signals to get the response.
     fn say(&mut self, message: String) {
         self.send_message(message);
     }
 
     #[signal]
+    /// Triggered when a new token is received from the LLM. Returns the new token as a string.
     fn response_updated(new_token: String);
 
     #[signal]
+    /// Triggered when the LLM has finished generating the response. Returns the full response as a string.
     fn response_finished(response: String);
 }
 
 #[derive(GodotClass)]
 #[class(base=Node)]
+/// The Embedding node is used to compare text. This is usefull as we cant predict excact sentences/triggerwords and thus want to compare how similar two sentences are.
+///
+/// This is done by embedding the text into a vector space and then comparing the cosine similarity between the vectors.
+///
+/// A good example of this would be to check if a npc has a trigger word The dragon is slain, and then reponds if the player says
+/// - The great worm is slain
+/// - I killed the dragon
+/// - The dragon is dead
+///
+/// Meaning you can reward the player for killing the dragon or trigger a new quest.
+/// It of course can also be used for other tasks such as checking if the player wants to buy or take something from the npc's inventory.
+///
+/// It requires a "NobodyWhoModel" node to be set with an embeddingsmodel in gguf format.
+/// Example:
+///
+/// ```
+/// extends NobodyWhoEmbedding
+///
+/// func _ready():
+///     # configure node
+///     self.model_node = get_node("../EmbeddingModel""""""")
+///
+///     # generate some embeddings
+///     embed("The dragon is on the hill.")
+///     var dragon_hill_embd = await self.embedding_finished
+///
+///     embed("The dragon is hungry for humans.")
+///     var dragon_hungry_embd = await self.embedding_finished
+///
+///     embed("This doesn't matter.")
+///     var irrelevant_embd = await self.embedding_finished
+///
+///     # test similarity,
+///     # here we show that two embeddings will have high similarity, if they mean similar things
+///     var low_similarity = cosine_similarity(irrelevant_embd, dragon_hill_embd)
+///     var high_similarity = cosine_similarity(dragon_hill_embd, dragon_hungry_embd)
+///     assert(low_similarity < high_similarity)
+/// ```
+///
 struct NobodyWhoEmbedding {
     #[export]
+    /// The model node for the embedding.
     model_node: Option<Gd<NobodyWhoModel>>,
 
     text_tx: Option<Sender<String>>,
@@ -318,6 +415,7 @@ impl INode for NobodyWhoEmbedding {
 #[godot_api]
 impl NobodyWhoEmbedding {
     #[signal]
+    /// Triggered when the embedding has finished. Returns the embedding as a PackedFloat32Array.
     fn embedding_finished(embedding: PackedFloat32Array);
 
     fn get_model(&mut self) -> Result<llm::Model, String> {
@@ -329,6 +427,7 @@ impl NobodyWhoEmbedding {
     }
 
     #[func]
+    /// Starts the embedding worker thread. This is required before you can send text to the embedding worker.
     fn start_worker(&mut self) {
         let mut result = || -> Result<(), String> {
             let model = self.get_model()?;
@@ -354,6 +453,10 @@ impl NobodyWhoEmbedding {
     }
 
     #[func]
+    /// Embeds a text into a vector space. This will return a signal that you can use to wait for the embedding.
+    /// The signal will return a PackedFloat32Array.
+    /// The Embed function is what takes a text and makes it into a vector. It can the be used in the `cosine_similarity` function to compare two vectors.
+    /// to compare agianst another previously embedded text.
     fn embed(&mut self, text: String) -> Signal {
         // returns signal, so that you can `var vec = await embed("Hello, world!")`
         if let Some(tx) = &self.text_tx {
@@ -370,6 +473,8 @@ impl NobodyWhoEmbedding {
     }
 
     #[func]
+    /// Calculates the cosine similarity between two embeddings ie. the similarity between two vectors.
+    /// Returns a value between 0 and 1, where 1 is the highest similarity.
     fn cosine_similarity(a: PackedFloat32Array, b: PackedFloat32Array) -> f32 {
         llm::cosine_similarity(a.as_slice(), b.as_slice())
     }
