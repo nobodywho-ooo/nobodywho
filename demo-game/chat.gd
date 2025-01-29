9bb706dd1978a96447fdea566682de0719832107
AsbjornOlling <asbjornolling@gmail.com>
2025-01-29 11:18:03 +0100

    clean up demo game

--- demo-game/chat.gd
+++ demo-game/chat.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	start_worker()
 
 func _on_response_updated(new_token: String) -> void:
-	%SpeechBubbleLabel.text += new_token
+	%SpeechBubbleLabel.text += new_token.replace("\n", "")
 
 func _on_response_finished(response: String) -> void:
 	# re-enable text input
@@ -15,6 +15,12 @@ func _on_response_finished(response: String) -> void:
 	%TextEdit.text = ""
 
 func _on_send_button_pressed() -> void:
+	await user_submitted_text()
+
+func _on_text_edit_text_submitted(new_text: String) -> void:
+	await user_submitted_text()
+
+func user_submitted_text():
 	# disable the input box
 	%TextEdit.editable = false
 	
@@ -29,21 +35,8 @@ func _on_send_button_pressed() -> void:
 	
 	# test if they asked to buy a potion
 	var potion = await match_sentence(%TextEdit.text)
-	await response_finished
 	if potion != null:
 		confirm_buy_potion(potion)
-
-func confirm_buy_potion(potion: String):
-	selected_potion = potion
-	if potion == "health_potion":
-		%ConfirmLabel.text = "Buy health potion for 3 gold?"
-	elif potion == "mana_potion":
-		%ConfirmLabel.text = "Buy mana potion for 3 gold?"
-	elif potion == "strength_potion":
-		%ConfirmLabel.text = "Buy strength potion for 5 gold?"
-	else:
-		assert(false) # unreachable, if we're ever here, it's a bug.
-	%ConfirmBox.visible = true
 
 func generate_potion_embeddings():
 	# generate embeddings for a few example sentences for each potion
@@ -51,6 +44,7 @@ func generate_potion_embeddings():
 	var sentences = {
 		"health_potion": [
 			"I'd like to buy the potion of minor healing.",
+			"I'd like to buy the health potion",
 			"I'd like to buy the potion in the round bottle.",
 			"Give me the health potion.",
 			"Buy the red potion."
@@ -58,13 +52,13 @@ func generate_potion_embeddings():
 		"mana_potion": [
 			"Give me the blue potion.",
 			"I'll get that mana potion.",
-			"I'd like the potion in the test tube.",
-			"I will purchase the potion in the skinny flask"
+			"I'd like the potion in the triangle flask.",
+			"I will purchase the potion in the triangle-shaped flask"
 		],
 		"strength_potion": [
 			"I'm gonna buy the strength potion.",
 			"Give me the orange potion.",
-			"I'd like the one in the square flask",
+			"I'd like the one in the test tube",
 			"I will purchase the potion that gives extra damage"
 		]
 	}
@@ -84,33 +78,40 @@ func match_sentence(sentence: String):
 	for potion in potion_embeddings:
 		for embedding in potion_embeddings[potion]:
 			var similarity = %NobodyWhoEmbedding.cosine_similarity(input_embed, embedding)
-			print(potion + " " + str(similarity))
 			if similarity > max_similarity:
 				most_similar = potion
 				max_similarity = similarity
-	var threshold = 0.8
+	var threshold = 0.85
 	if max_similarity > threshold:
-		print("MOST SIMILAR: " + most_similar)
 		return most_similar
 	return null
 
-
-func buy_selected_potion():
-	if selected_potion == "health_potion":
-		pass
-	elif selected_potion == "mana_potion":
-		pass
-	elif selected_potion == "strength_potion":
-		pass
-	say("*user buys a " + selected_potion + "*")
-	%SpeechBubbleLabel.text = ""
-	selected_potion = null
+func confirm_buy_potion(potion: String):
+	selected_potion = potion
+	if potion == "health_potion":
+		%ConfirmLabel.text = "Buy health potion for 3 gold?"
+	elif potion == "mana_potion":
+		%ConfirmLabel.text = "Buy mana potion for 3 gold?"
+	elif potion == "strength_potion":
+		%ConfirmLabel.text = "Buy strength potion for 3 gold?"
+	else:
+		assert(false) # unreachable, if we're ever here, it's a bug.
+	%ConfirmBox.visible = true
 
 func _on_yes_button_pressed() -> void:
-	buy_selected_potion()
+	if selected_potion == "health_potion":
+		%HealthPotionSprite.visible = false
+	elif selected_potion == "mana_potion":
+		%ManaPotionSprite.visible = false
+	elif selected_potion == "strength_potion":
+		%StrengthPotionSprite.visible = false
+	
+	%SpeechBubbleLabel.text = ""
 	%ConfirmBox.visible = false
 
+	say("*user has confirmed buying a " + selected_potion + "*")
+	selected_potion = null
 
 func _on_no_button_pressed() -> void:
 	%ConfirmBox.visible = false
-	selected_potion = false
+	selected_potion = null
