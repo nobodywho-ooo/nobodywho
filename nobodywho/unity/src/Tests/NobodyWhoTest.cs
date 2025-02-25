407a0e7ba9aaab2e3bfacad81c5e607129e4770e
norsker <emilgraaenorsker@gmail.com>
2025-02-25 09:47:13 +0100

    feat: minimal unity example

---
+++ nobodywho/unity/src/Tests/NobodyWhoTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+namespace NobodyWho.Tests
+{
+    public class NobodyWhoTest
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            // Example test
+            Assert.Pass();
+        }
+    }
+} 
