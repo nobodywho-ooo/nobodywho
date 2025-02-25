bc85b10a67bdc29739b008016799b027ad7dd29a
norsker <emilgraaenorsker@gmail.com>
2025-02-25 16:25:30 +0100

    feat: restructure nix setup

--- nobodywho/default.nix
+++ nobodywho/default.nix
@@ -1,48 +1,20 @@
-{ fetchurl, rustPlatform, libclang, llvmPackages_12, stdenv, lib, cmake, vulkan-headers, vulkan-loader, vulkan-tools, shaderc, mesa }:
+{ pkgs, lib }:
 
 
-rustPlatform.buildRustPackage {
-  pname = "nobody";
-  version = "0.0.0";
-  src = ./.;
-  nativeBuildInputs = [
-    llvmPackages_12.bintools
-    cmake
-    rustPlatform.bindgenHook
-    vulkan-headers
-    vulkan-loader
-    shaderc
-    vulkan-tools
-    mesa.drivers
-  ];
-  buildInputs = [
-    vulkan-loader
-    vulkan-headers
-    shaderc
-    vulkan-tools
-    mesa.drivers
-  ];
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-    outputHashes = {
-      "gdextension-api-0.2.1" = "sha256-YkMbzObJGnmQa1XGT4ApRrfqAeOz7CktJrhYks8z0RY=";
-      "godot-0.2.2" = "sha256-6q7BcQ/6WvzJdVmyAVGPMtuIDJFYKaRrkv3/JQBi11M=";
-      "llama-cpp-2-0.1.90" = "sha256-SUY4Hb2DAjDonQMEIyKXoXRygxBh/M+CeyQBviqg46g=";
-    };
-  };
-  env.TEST_MODEL = fetchurl {
-    name = "qwen2.5-1.5b-instruct-q4_0.gguf";
-    url = "https://huggingface.co/Qwen/Qwen2.5-1.5B-Instruct-GGUF/resolve/main/qwen2.5-1.5b-instruct-q4_0.gguf";
-    hash = "sha256-3NgZ/wlIUsOPq6aHPY/wydUerbKERTnlIEKuXWR7v9s=";
-  };
-  env.TEST_EMBEDDINGS_MODEL = fetchurl {
-    name = "bge-small-en-v1.5-q8_0.gguf";
-    url = "https://huggingface.co/CompendiumLabs/bge-small-en-v1.5-gguf/resolve/main/bge-small-en-v1.5-q8_0.gguf";
-    sha256 = "sha256-7Djo2hQllrqpExJK5QVQ3ihLaRa/WVd+8vDLlmDC9RQ=";
-  };
+# Techdept: 
+#  Currently this sytem uis rebuilding all dependencies for every build. Nothing is cached and
+#  the godot and unity package will have to be rebuild whenever there is a change to any of the packages.
+# some of this is due to the usage of rustPlatform.buildRustPackage
+# we have tried to use crane, but there is a bug in the build.rs of rustbindings (so it requires upstream fix to either llama.cpp, rustbindings or crane)
+let
+  nobodywho = pkgs.callPackage ./core { };
+  godot = pkgs.callPackage ./godot { inherit nobodywho; };
 
-  checkPhase = ''
-    cargo test -- --test-threads=1 --nocapture
-  '';
-  doCheck = true;
+in {
+  packages = {
+    default = nobodywho;
+    godot = godot.packages.default;
+  };
+  
+  checks = lib.mapAttrs (name: value: value) godot.checks;
 }
--- nobodywho/godot/default.nix
+++ nobodywho/godot/default.nix
@@ -1,51 +1,72 @@
-{ fetchurl, rustPlatform, libclang, llvmPackages_12, stdenv, lib, cmake, vulkan-headers, vulkan-loader, vulkan-tools, shaderc, mesa, git }:
+{ pkgs, nobodywho, fetchurl, rustPlatform, libclang, llvmPackages_12, godot_4, godot_4-export-templates, stdenv, lib, cmake, vulkan-headers, vulkan-loader, vulkan-tools, shaderc, mesa }:
 
-
-rustPlatform.buildRustPackage {
-  pname = "nobody";
-  version = "0.0.0";
-  src = ./..;
-  buildAndTestSubdir = "godot";
-  nativeBuildInputs = [
-    git
-    llvmPackages_12.bintools
-    cmake
-    rustPlatform.bindgenHook
-    rustPlatform.cargoBuildHook
-    vulkan-headers
-    vulkan-loader
-    shaderc
-    vulkan-tools
-    mesa
-  ];
-  buildInputs = [
-    vulkan-loader
-    vulkan-headers
-    shaderc
-    vulkan-tools
-    mesa
-  ];
-  cargoLock = {
-    lockFile = ../Cargo.lock;
-    outputHashes = {
-      "gdextension-api-0.2.2" = "sha256-gaxM73OzriSDm6tLRuMTOZxCLky9oS1nq6zTsm0g4tA=";
-      "godot-0.2.4" = "sha256-bTLqnwYZJBBAIoEPVAOVIo2SM3oIC537fjRHAG5w5fE=";
-      "llama-cpp-2-0.1.103" = "sha256-T8qxxAC0ygF655EzODIpDjIKS0vRMe68e5rJcP1+PDo=";
+let
+  godot = rustPlatform.buildRustPackage {
+    pname = "godot";
+    version = "0.0.0";
+    src = ./..;
+    buildAndTestSubdir = "godot";
+    nativeBuildInputs = [
+      llvmPackages_12.bintools
+      cmake
+      rustPlatform.bindgenHook
+      rustPlatform.cargoBuildHook
+      vulkan-headers
+      vulkan-loader
+      shaderc
+      vulkan-tools
+      mesa
+    ];
+    buildInputs = [
+      nobodywho
+      vulkan-loader
+      vulkan-headers
+      shaderc
+      vulkan-tools
+      mesa
+    ];
+    cargoLock = {
+      lockFile = ../Cargo.lock;
+      outputHashes = {
+        "gdextension-api-0.2.1" = "sha256-YkMbzObJGnmQa1XGT4ApRrfqAeOz7CktJrhYks8z0RY=";
+        "godot-0.2.2" = "sha256-6q7BcQ/6WvzJdVmyAVGPMtuIDJFYKaRrkv3/JQBi11M=";
+        "llama-cpp-2-0.1.90" = "sha256-SUY4Hb2DAjDonQMEIyKXoXRygxBh/M+CeyQBviqg46g=";
+      };
     };
-  };
-  env.TEST_MODEL = fetchurl {
-    name = "qwen2.5-1.5b-instruct-q4_0.gguf";
-    url = "https://huggingface.co/Qwen/Qwen2.5-1.5B-Instruct-GGUF/resolve/main/qwen2.5-1.5b-instruct-q4_0.gguf";
-    hash = "sha256-3NgZ/wlIUsOPq6aHPY/wydUerbKERTnlIEKuXWR7v9s=";
-  };
-  env.TEST_EMBEDDINGS_MODEL = fetchurl {
-    name = "bge-small-en-v1.5-q8_0.gguf";
-    url = "https://huggingface.co/CompendiumLabs/bge-small-en-v1.5-gguf/resolve/main/bge-small-en-v1.5-q8_0.gguf";
-    sha256 = "sha256-7Djo2hQllrqpExJK5QVQ3ihLaRa/WVd+8vDLlmDC9RQ=";
+    env.TEST_MODEL = fetchurl {
+      name = "qwen2.5-1.5b-instruct-q4_0.gguf";
+      url = "https://huggingface.co/Qwen/Qwen2.5-1.5B-Instruct-GGUF/resolve/main/qwen2.5-1.5b-instruct-q4_0.gguf";
+      hash = "sha256-3NgZ/wlIUsOPq6aHPY/wydUerbKERTnlIEKuXWR7v9s=";
+    };
+    env.TEST_EMBEDDINGS_MODEL = fetchurl {
+      name = "bge-small-en-v1.5-q8_0.gguf";
+      url = "https://huggingface.co/CompendiumLabs/bge-small-en-v1.5-gguf/resolve/main/bge-small-en-v1.5-q8_0.gguf";
+      sha256 = "sha256-7Djo2hQllrqpExJK5QVQ3ihLaRa/WVd+8vDLlmDC9RQ=";
+    };
+    checkPhase = ''
+      cargo test -- --test-threads=1 --nocapture
+    '';
+    doCheck = true;
   };
 
-  checkPhase = ''
-    cargo test -- --test-threads=1 --nocapture
+  integration-test = pkgs.callPackage ./integration-test {
+    inherit godot;  # Pass the godot package we just built
+  };
+
+  run-integration-test = pkgs.runCommand "checkgame" {
+    nativeBuildInputs = [ mesa.drivers ];
+  } ''
+    cd ${integration-test}
+    export HOME=$TMPDIR
+    ./game --headless
+    touch $out
   '';
-  doCheck = true;
+
+in {
+  packages = {
+    default = godot;
+  };
+  checks = {
+    godot-integration-test = run-integration-test;
+  };
 }
