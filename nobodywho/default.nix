7e80431ab03f0f46d0d4556518cc50b5f20ccbc6
norsker <emilgraaenorsker@gmail.com>
2025-02-27 14:17:04 +0100

    refactor nix

--- nobodywho/default.nix
+++ nobodywho/default.nix
@@ -1,20 +1,35 @@
 { pkgs, lib }:
 
-
 # Techdept: 
-#  Currently this sytem uis rebuilding all dependencies for every build. Nothing is cached and
+#  Currently this system is rebuilding all dependencies for every build. Nothing is cached and
 #  the godot and unity package will have to be rebuild whenever there is a change to any of the packages.
 # some of this is due to the usage of rustPlatform.buildRustPackage
 # we have tried to use crane, but there is a bug in the build.rs of rustbindings (so it requires upstream fix to either llama.cpp, rustbindings or crane)
 let
-  nobodywho = pkgs.callPackage ./core { };
-  godot = pkgs.callPackage ./godot { inherit nobodywho; };
-
-in {
-  packages = {
-    default = nobodywho;
-    godot = godot.packages.default;
+  # Import core first
+  core = pkgs.callPackage ./core {};
+  
+  # # Pass the core package to godot
+  # godot = pkgs.callPackage ./godot { 
+  #   inherit core;
+  # };
+  
+  # Import unity module
+  unity = pkgs.callPackage ./unity { 
+    inherit core;
   };
   
-  checks = lib.mapAttrs (name: value: value) godot.checks;
+in {
+  core = core.nobodywho;
+  unity = unity.unity_editor;
+  # packages = {
+  #   # Export each package with the correct name
+  #   #godot = godot.packages.godot;
+  #   #unity = unity.unity_editor;
+  # };
+  
+  # Merge checks from all modules
+  checks = (lib.mapAttrs (name: value: value) unity.checks);
+    # (lib.mapAttrs (name: value: value) godot.checks)
+    
 }
