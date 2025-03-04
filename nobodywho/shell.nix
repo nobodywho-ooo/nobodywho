27cce3823cb43a82c8ae1b75534c17d4fd5479da
norsker <emilgraaenorsker@gmail.com>
2025-03-04 16:08:59 +0100

    unity pain semi over

--- nobodywho/shell.nix
+++ nobodywho/shell.nix
@@ -1,10 +1,21 @@
 { pkgs ? import <nixpkgs> {}, ... }: 
-pkgs.mkShell {
+
+let 
+  nobodywho = pkgs.callPackage ./default.nix {};
+  unity-editor = nobodywho.unity-editor;
+
+in pkgs.mkShell {
   env.LIBCLANG_PATH = "${pkgs.libclang.lib}/lib/libclang.so";
   packages = [
     pkgs.cmake
     pkgs.clang
     pkgs.rustup
+    
+    # Unity
+    unity-editor
+    pkgs.unityhub
+    pkgs.libxml2
+    pkgs.just
 
     # these are the dependencies required by llama.cpp to build for vulkan
     # (these packages were discovered by looking at the nix source code in ggerganov/llama.cpp)
