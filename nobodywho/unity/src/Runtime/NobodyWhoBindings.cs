3da13dc18f935745478d4c880f602136e140fd09
norsker <emilgraaenorsker@gmail.com>
2025-03-05 15:07:12 +0100

    fix: error handling

--- nobodywho/unity/src/Runtime/NobodyWhoBindings.cs
+++ nobodywho/unity/src/Runtime/NobodyWhoBindings.cs
@@ -3,6 +3,22 @@ using System.Runtime.InteropServices;
 
 namespace NobodyWho
 {
+    public enum ModelErrorType
+    {
+        ModelNotFound = 1,
+        InvalidModel = 2
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct ModelResult
+    {
+        public IntPtr handle;
+        [MarshalAs(UnmanagedType.I1)]
+        public bool success;
+        public ModelErrorType errorType;
+        public IntPtr errorMessage;
+    }
+
     /// <summary>
     /// Native bindings to the Rust library functions
     /// </summary>
@@ -11,10 +27,8 @@ namespace NobodyWho
         private const string LIB_NAME = "libnobodywho";  // Will be libnobodywho.so on Linux, libnobodywho.dylib on Mac, nobodywho.dll on Windows
 
         [DllImport(LIB_NAME, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr get_model(
+        public static extern ModelResult get_model(
             [MarshalAs(UnmanagedType.LPUTF8Str)] string model_path,
             bool use_gpu);
-
     }
-
 } 
