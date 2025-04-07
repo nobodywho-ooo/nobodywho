b0d4708a7193f10ac3a68835cce03646d1d89a0b
norsker <emilgraaenorsker@gmail.com>
2025-04-07 09:59:14 +0200

    refactor: reimplement models tests
    they were commented out to make iteration loop smaller

--- nobodywho/unity/src/Tests/NobodyWhoModelTests.cs
+++ nobodywho/unity/src/Tests/NobodyWhoModelTests.cs
@@ -30,36 +30,36 @@ namespace Tests
             }
         }
 
-        // [Test]
-        // public void WhenModelIsWrong_ShouldThrowNobodyWhoException()
-        // {
-        //     // Create a fake GGUF file with invalid content
-        //     string tempPath = Path.Combine(Application.streamingAssetsPath, "invalid.gguf");
-        //     File.WriteAllText(tempPath, "This is not a valid GGUF file");
+        [Test]
+        public void WhenModelIsWrong_ShouldThrowNobodyWhoException()
+        {
+            // Create a fake GGUF file with invalid content
+            string tempPath = Path.Combine(Application.streamingAssetsPath, "invalid.gguf");
+            File.WriteAllText(tempPath, "This is not a valid GGUF file");
             
-        //     try 
-        //     {
-        //         model.modelPath = tempPath;
-        //         var exception = Assert.Throws<NobodyWhoException>(() => model.GetModel());
-        //     }
-        //     finally 
-        //     {
-        //         // Ensure we clean up the temp file even if the test fails
-        //         if (File.Exists(tempPath))
-        //         {
-        //             File.Delete(tempPath);
-        //         }
-        //     }
-        // }
+            try 
+            {
+                model.modelPath = tempPath;
+                var exception = Assert.Throws<NobodyWhoException>(() => model.GetModel());
+            }
+            finally 
+            {
+                // Ensure we clean up the temp file even if the test fails
+                if (File.Exists(tempPath))
+                {
+                    File.Delete(tempPath);
+                }
+            }
+        }
     
         
-        // [Test] 
-        // public void WhenModelPathIsGGUF_ShouldLoadModel()
-        // {
-        //     // TODO: add a build step in nix for the model in `create temp project`. otherwise this will fail when other people run it.
-        //     model.modelPath = "qwen2.5-1.5b-instruct-q4_0.gguf";
-        //     var model_handle = model.GetModel();
-        //     Assert.That(model_handle, Is.Not.EqualTo(IntPtr.Zero));
-        // }
+        [Test] 
+        public void WhenModelPathIsGGUF_ShouldLoadModel()
+        {
+            // TODO: add a build step in nix for the model in `create temp project`. otherwise this will fail when other people run it.
+            model.modelPath = "qwen2.5-1.5b-instruct-q4_0.gguf";
+            var model_handle = model.GetModel();
+            Assert.That(model_handle, Is.Not.EqualTo(IntPtr.Zero));
+        }
     }
 } 
