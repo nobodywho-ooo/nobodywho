5391b477e552930970e6033b842e5ab41cb67537
norsker <emilgraaenorsker@gmail.com>
2025-04-10 10:20:19 +0200

    refactor: embedding callbacks to be passed to rs

--- nobodywho/unity/src/Runtime/NobodyWhoEmbedding.cs
+++ nobodywho/unity/src/Runtime/NobodyWhoEmbedding.cs
@@ -13,14 +13,69 @@ namespace NobodyWho {
         public UnityEvent<string> onError = new UnityEvent<string>();
 
         private AwaitableCompletionSource<float[]> _embeddingSignal;
+        private GCHandle _gcHandle;
 
-        private void OnEmbedding(IntPtr data, int length) {
+        // we need a reference to the callback to keep it alive and not GC'ed. 
+        private static NativeBindings.EmbeddingCallback _embeddingCallback = OnEmbeddingCallback;
+        // This is a callback that is invoked when the embedding is complete generating.
+        // the AOT (ahead of time compile ) is required for macos as their security model does not allow JIT.
+        // the P/invoke marks this as a callback that can be called from native code to avoid it being optimized away.
+        [AOT.MonoPInvokeCallback(typeof(NativeBindings.EmbeddingCallback))]
+        private static void OnEmbeddingCallback(IntPtr caller, IntPtr data, int length) {
+            GCHandle handle = GCHandle.FromIntPtr(caller);
+            Embedding instance = handle.Target as Embedding;
+            
             float[] embedding = new float[length];
             Marshal.Copy(data, embedding, 0, length);
             
-            _embeddingSignal?.SetResult(embedding);
+            instance._embeddingSignal?.SetResult(embedding);
+            instance.onEmbeddingComplete.Invoke(embedding);
+        }
+
+        void Start() {
+            _gcHandle = GCHandle.Alloc(this);
+            var errorBuffer = new StringBuilder(2048);
             
-            onEmbeddingComplete.Invoke(embedding);
+            _actorContext = NativeBindings.create_embedding_worker(
+                model.GetModel(),
+                GCHandle.ToIntPtr(_gcHandle),
+                OnEmbeddingCallback,
+                errorBuffer
+            );
+            
+            if (errorBuffer.Length > 0) {
+                Debug.LogError(errorBuffer.ToString());
+                enabled = false;
+            }
+        }
+
+        public Awaitable<float[]> Embed(string text) {
+            Debug.Log("[DEBUG] Embedding: " + text);
+            _embeddingSignal = new AwaitableCompletionSource<float[]>();
+            
+            var errorBuffer = new StringBuilder(2048);
+            Debug.Log("[DEBUG] Embedding via: " + _textTransmitter);
+            NativeBindings.embed_text(_actorContext, text, errorBuffer);
+            
+            if (errorBuffer.Length > 0) {
+                Debug.LogError(errorBuffer.ToString());
+            }
+            
+            return _embeddingSignal.Awaitable;
+        }
+
+        // This has several responsibilites:
+        // 1. kill the embedding worker.
+        // 2. kill the gc handle.
+        // 3. deref the model strong count by 1.
+        void OnDestroy() {
+            if (_actorContext != IntPtr.Zero) {
+                NativeBindings.destroy_embedding_worker(_actorContext);
+            }
+            
+            if (_gcHandle.IsAllocated) {
+                _gcHandle.Free();
+            }
         }
 
         private void OnError(string error) {
@@ -28,53 +83,6 @@ namespace NobodyWho {
             
             onError.Invoke(error);
             Debug.LogError($"Embedding Error: {error}");
-        }
-
-        void Start() {
-            try {
-                var errorBuffer = new StringBuilder(2048);
-                _workerContext = NativeBindings.create_embedding_worker(
-                    model.GetModel(),
-                    errorBuffer
-                );
-                
-                if (errorBuffer.Length > 0) {
-                    throw new NobodyWhoException(errorBuffer.ToString());
-                }
-            } catch (Exception e) {
-                throw new NobodyWhoException(e.Message);
-            }
-        }
-
-        void Update() {
-            if (_workerContext == IntPtr.Zero) {
-                return;
-            }
-            try {
-                NativeBindings.poll_embeddings(
-                    _workerContext,
-                    OnEmbedding,
-                    OnError
-                );
-            } catch (Exception e) {
-                throw new NobodyWhoException(e.Message);
-            }
-        }
-
-        public Awaitable<float[]> Embed(string text) {
-            try {
-                var errorBuffer = new StringBuilder(2048);
-                _embeddingSignal = new AwaitableCompletionSource<float[]>();
-                
-                NativeBindings.embed_text(_workerContext, text, errorBuffer);
-                if (errorBuffer.Length > 0) {
-                    throw new NobodyWhoException(errorBuffer.ToString());
-                }
-
-                return _embeddingSignal.Awaitable;
-            } catch (Exception e) {
-                throw new NobodyWhoException(e.Message);
-            }
         }
 
         public float CosineSimilarity(float[] a, float[] b) {
