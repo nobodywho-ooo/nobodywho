1146cad1d75124195c369ae85b5c5fe76d47dcb0
norsker <emilgraaenorsker@gmail.com>
2025-04-14 14:53:20 +0200

    fix: readd all the tests

--- nobodywho/unity/src/Tests/NobodyWhoEmbeddingTests.cs
+++ nobodywho/unity/src/Tests/NobodyWhoEmbeddingTests.cs
@@ -33,50 +33,50 @@ namespace Tests
             }
         }
 
-        // [Test]
-        // public async Task WhenComparingEmbeddings_SimilarTextsShouldHaveHigherSimilarity()
-        // {
-        //     Debug.Log(
-        //         "[DEBUG] Starting embedding test, embedding model: " + embedding.model.modelPath
-        //     );
-        //     float[] dragonHillEmbedding = await embedding.Embed("The dragon is on the hill.");
-        //     Assert.IsNotNull(dragonHillEmbedding, "First embedding not received");
-        //     Debug.Log("[DEBUG] First embedding received: " + dragonHillEmbedding);
-        //     float[] dragonHungryEmbedding = await embedding.Embed(
-        //         "The dragon is hungry for humans."
-        //     );
-        //     Assert.IsNotNull(dragonHungryEmbedding, "Second embedding not received");
-        //     Debug.Log("[DEBUG] Second embedding received: " + dragonHungryEmbedding);
-        //     float[] unrelatedEmbedding = await embedding.Embed("This does not matter.");
-        //     Assert.IsNotNull(unrelatedEmbedding, "Third embedding not received");
+        [Test]
+        public async Task WhenComparingEmbeddings_SimilarTextsShouldHaveHigherSimilarity()
+        {
+            Debug.Log(
+                "[DEBUG] Starting embedding test, embedding model: " + embedding.model.modelPath
+            );
+            float[] dragonHillEmbedding = await embedding.Embed("The dragon is on the hill.");
+            Assert.IsNotNull(dragonHillEmbedding, "First embedding not received");
+            Debug.Log("[DEBUG] First embedding received: " + dragonHillEmbedding);
+            float[] dragonHungryEmbedding = await embedding.Embed(
+                "The dragon is hungry for humans."
+            );
+            Assert.IsNotNull(dragonHungryEmbedding, "Second embedding not received");
+            Debug.Log("[DEBUG] Second embedding received: " + dragonHungryEmbedding);
+            float[] unrelatedEmbedding = await embedding.Embed("This does not matter.");
+            Assert.IsNotNull(unrelatedEmbedding, "Third embedding not received");
 
-        //     Debug.Log(
-        //         "Embedding complete: "
-        //             + dragonHillEmbedding
-        //             + " "
-        //             + dragonHungryEmbedding
-        //             + " "
-        //             + unrelatedEmbedding
-        //     );
+            Debug.Log(
+                "Embedding complete: "
+                    + dragonHillEmbedding
+                    + " "
+                    + dragonHungryEmbedding
+                    + " "
+                    + unrelatedEmbedding
+            );
 
-        //     Assert.IsNotNull(dragonHillEmbedding, "First embedding not received");
-        //     Assert.IsNotNull(dragonHungryEmbedding, "Second embedding not received");
-        //     Assert.IsNotNull(unrelatedEmbedding, "Third embedding not received");
+            Assert.IsNotNull(dragonHillEmbedding, "First embedding not received");
+            Assert.IsNotNull(dragonHungryEmbedding, "Second embedding not received");
+            Assert.IsNotNull(unrelatedEmbedding, "Third embedding not received");
 
-        //     float lowSimilarity = embedding.CosineSimilarity(
-        //         unrelatedEmbedding,
-        //         dragonHillEmbedding
-        //     );
-        //     float highSimilarity = embedding.CosineSimilarity(
-        //         dragonHillEmbedding,
-        //         dragonHungryEmbedding
-        //     );
+            float lowSimilarity = embedding.CosineSimilarity(
+                unrelatedEmbedding,
+                dragonHillEmbedding
+            );
+            float highSimilarity = embedding.CosineSimilarity(
+                dragonHillEmbedding,
+                dragonHungryEmbedding
+            );
 
-        //     Assert.Greater(
-        //         highSimilarity,
-        //         lowSimilarity,
-        //         $"Similar texts should have higher similarity. Low: {lowSimilarity}, High: {highSimilarity}"
-        //     );
-        // }
+            Assert.Greater(
+                highSimilarity,
+                lowSimilarity,
+                $"Similar texts should have higher similarity. Low: {lowSimilarity}, High: {highSimilarity}"
+            );
+        }
     }
 }
