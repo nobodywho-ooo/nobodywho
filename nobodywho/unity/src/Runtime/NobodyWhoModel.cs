9d8d942c9bf9138e85ea8de98eb0b4919509a696
AsbjornOlling <asbjornolling@gmail.com>
2025-05-16 15:39:21 +0200

    new bindings, new nobodywhomodel

--- nobodywho/unity/src/Runtime/NobodyWhoModel.cs
+++ nobodywho/unity/src/Runtime/NobodyWhoModel.cs
@@ -9,45 +9,20 @@ namespace NobodyWho
 {
     public class Model : MonoBehaviour
     {
-        public string modelPath = "model.gguf";
+        private ModelWrapper wrapper = ModelWrapper.New("model.gguf", true);
 
-        public bool useGpuIfAvailable = true;
-        private IntPtr modelHandle;
-
-        public IntPtr GetModel()
-        {
-            try
-            {
-                string fullPath = Path.Combine(Application.streamingAssetsPath, modelPath);
-                var errorBuffer = new StringBuilder(2048); // update lib.rs if you change this value
-                var result = NativeBindings.get_model(
-                    modelHandle,
-                    fullPath,
-                    useGpuIfAvailable,
-                    errorBuffer
-                );
-
-                if (result == IntPtr.Zero)
-                {
-                    throw new NobodyWhoException(errorBuffer.ToString());
-                }
-
-                modelHandle = result;
-                return modelHandle;
-            }
-            catch (Exception e)
-            {
-                throw new NobodyWhoException(e.Message);
-            }
+        public string ModelPath {
+            get { return Marshal.PtrToStringAnsi(wrapper.GetModelPath()); }
+            set { wrapper.SetModelPath(value); }
         }
 
-        void OnDestroy()
-        {
-            // we cant destroy something that does not exist
-            if (modelHandle != IntPtr.Zero)
-            {
-                NativeBindings.destroy_model(modelHandle);
-            }
+        public bool UseGpuIfAvailable {
+            get { return wrapper.GetUseGpuIfAvailable(); }
+            set { wrapper.SetUseGpuIfAvailable(value); }
+        }
+
+        public IntPtr ModelWrapperContext {
+            get { return wrapper.Context; }
         }
     }
 }
