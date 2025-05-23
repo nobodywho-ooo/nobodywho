10945d53b8684a61377a851b8d6fa93c7c019028
norsker <emilgraaenorsker@gmail.com>
2025-05-23 14:04:05 +0200

    feat: update base image to 24.04

--- .github/custom-ci-images/build-images.sh
+++ .github/custom-ci-images/build-images.sh
@@ -11,7 +11,20 @@ docker build -t local-gameci-base:latest ubuntu/base/
 docker build --build-arg baseImage=local-gameci-base:latest -t local-gameci-hub:latest ubuntu/hub/
 
 # Build editor image
-docker build --build-arg baseImage=local-gameci-base:latest --build-arg hubImage=local-gameci-hub:latest -t local-gameci-editor:latest ubuntu/editor/
+docker build \
+    --build-arg baseImage=local-gameci-base:latest \
+    --build-arg hubImage=local-gameci-hub:latest \
+    --build-arg version=6000.0.47f1 \
+    --build-arg changeSet=8a060a5ff2be \
+    --build-arg module=linux-il2cpp \
+    -t local-gameci-editor:latest \
+    ubuntu/editor/
 
 # Build test-runner-vulkan image
-docker build -t local-gameci-testrunner-vulkan:latest -f gameci-test-runner-vulkan .
+docker build -t nobodywho-unity-ci:latest -f gameci-test-runner-vulkan .
+
+
+# if [ -n "$RELEASE" ]; then
+#    
+#     docker push nobodywho/nobodywho-unity-ci:latest
+# fi
