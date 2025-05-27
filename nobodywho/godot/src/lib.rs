204a3a9d6cb3bd82fb1d0e7a3e5fb510b779ea62
norsker <emilgraaenorsker@gmail.com>
2025-05-27 13:29:41 +0200

    fix: unsafe for extensionlibrary for godot

--- nobodywho/godot/src/lib.rs
+++ nobodywho/godot/src/lib.rs
@@ -10,7 +10,7 @@ use crate::sampler_resource::NobodyWhoSampler;
 struct NobodyWhoExtension;
 
 #[gdextension]
-impl ExtensionLibrary for NobodyWhoExtension {}
+unsafe impl ExtensionLibrary for NobodyWhoExtension {}
 
 #[derive(GodotClass)]
 #[class(base=Node)]
