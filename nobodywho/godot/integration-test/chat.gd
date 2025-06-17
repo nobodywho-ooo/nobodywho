f04cf852a0a714232dd099e10c2dc96b467f9856
AsbjornOlling <asbjornolling@gmail.com>
2025-06-17 19:44:19 +0200

    merge integration test too

--- nobodywho/godot/integration-test/chat.gd
+++ nobodywho/godot/integration-test/chat.gd
@@ -10,11 +10,8 @@ func run_test():
 	assert(await test_antiprompts())
 	assert(await test_antiprompts_multitokens())
 	assert(await test_chat_history())
-<<<<<<< HEAD
 	assert(await test_stop_generation())
-=======
 	assert(await test_tool_call())
->>>>>>> main
 	return true
 
 func test_say():
