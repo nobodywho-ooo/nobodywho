d28da44c9f5de18d667557ad88392139ebae1062
AsbjornOlling <asbjornolling@gmail.com>
2025-06-18 10:58:26 +0200

    clean up stopping test properly

--- nobodywho/godot/integration-test/chat.gd
+++ nobodywho/godot/integration-test/chat.gd
@@ -10,8 +10,8 @@ func run_test():
 	assert(await test_antiprompts())
 	assert(await test_antiprompts_multitokens())
 	assert(await test_chat_history())
-	assert(await test_tool_call())
 	assert(await test_stop_generation())
+	assert(await test_tool_call())
 	return true
 
 func test_say():
@@ -107,7 +107,7 @@ func test_tool_call():
 func test_stop_generation():
 	print("✨ Testing stop generation")
 	system_prompt = "You're countbot. A robot that's very good at counting"
-	start_worker()
+	reset_context()
 
 	# XXX: this signal is never disconnected
 	self.response_updated.connect(func(token: String):
@@ -121,5 +121,9 @@ func test_stop_generation():
 	print("✨ Got response: " + response)
 	assert("2" in response, "Should stop at 2")
 	assert(not "8" in response, "Should not continue past 2")
-	
+
+	# clean up: disconnect signal handler
+	for dict in response_updated.get_connections():
+		response_updated.disconnect(dict.callable)
+
 	return true
