c17db9468807f4f4f007d7aec41ca1e9b51816ab
AsbjornOlling <asbjornolling@gmail.com>
2025-07-29 15:05:26 +0200

    compile for aarch64-linux-android works

--- nobodywho/shell.nix
+++ nobodywho/shell.nix
@@ -1,18 +1,37 @@
-{ pkgs ? import <nixpkgs> {}, ... }: 
+{
+  pkgs ? import <nixpkgs> { },
+  android-nixpkgs,
+  ...
+}:
 
-let 
-  version = "6000.0.47f1";
+let
+  unity_version = "6000.0.47f1";
   unity-editor = pkgs.writeShellScriptBin "unity-editor" ''
-    ${pkgs.lib.getExe pkgs.unityhub.fhsEnv} ~/Unity/Hub/Editor/${version}/Editor/Unity "$@"
+    ${pkgs.lib.getExe pkgs.unityhub.fhsEnv} ~/Unity/Hub/Editor/${unity_version}/Editor/Unity "$@"
   '';
-
-in pkgs.mkShell {
+  android-sdk = android-nixpkgs.sdk.x86_64-linux (
+    sdkPkgs: with sdkPkgs; [
+      cmdline-tools-latest
+      build-tools-34-0-0
+      platform-tools
+      platforms-android-34
+      emulator
+      # weird hack to get NDK in here
+      # see: https://github.com/tadfisher/android-nixpkgs/issues/113#issuecomment-3112489517
+      (sdkPkgs."ndk-23-2-8568313".overrideAttrs (old: {
+        autoPatchelfIgnoreMissingDeps = true;
+      }))
+    ]
+  );
+in
+pkgs.mkShell {
   env.LIBCLANG_PATH = "${pkgs.libclang.lib}/lib/libclang.so";
+
   packages = [
     pkgs.cmake
     pkgs.clang
     pkgs.rustup
-    
+
     # Unity
     unity-editor
     pkgs.unityhub
@@ -31,6 +50,10 @@ in pkgs.mkShell {
     pkgs.vulkan-loader
     pkgs.shaderc
 
+    # for android
+    android-sdk
+    pkgs.openjdk11-bootstrap
+
     # for mkdocs
     pkgs.mkdocs
     pkgs.python312Packages.regex
@@ -38,5 +61,9 @@ in pkgs.mkShell {
   ];
   shellHook = ''
     ulimit -n 2048
+
+    # https://godot-rust.github.io/book/toolchain/export-android.html
+    export ANDROID_NDK="$ANDROID_SDK_ROOT/ndk/23.2.8568313"
+    export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$ANDROID_SDK_ROOT/ndk/23.2.8568313/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android31-clang"
   '';
 }
