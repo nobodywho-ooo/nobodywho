e781132210879d0f798a14a9f2c15cf4fcf7f96f
AsbjornOlling <asbjornolling@gmail.com>
2025-09-18 15:28:15 +0200

    handle async tools

--- nobodywho/flutter_plugin/lib/nobodywho_flutter.dart
+++ nobodywho/flutter_plugin/lib/nobodywho_flutter.dart
@@ -15,13 +15,19 @@ NobodyWhoTool toolFromFunction({required Function function, required String name
   // and to deal with dynamic function parameters
 
   // make it a
-  final wrappedfunction = (String jsonString) {
+  final wrappedfunction = (String jsonString) async {
     Map<String, dynamic> jsonMap = json.decode(jsonString);
     Map<Symbol, dynamic> namedParams = Map.fromEntries(
       jsonMap.entries.map((e) => MapEntry(Symbol(e.key), e.value))
     );
     
-    return Function.apply(function, [], namedParams).toString();
+    final result = Function.apply(function, [], namedParams);
+
+    if (result is Future) {
+      return (await result).toString();
+    } else {
+      return result.toString();
+    }
   };
 
   return newToolImpl(function: wrappedfunction, name: name, description: description, runtimeType: function.runtimeType.toString());
