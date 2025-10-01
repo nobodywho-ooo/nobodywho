1e6a12700a7df4d59a6eed0b516d734f9b560ae0
AsbjornOlling <asbjornolling@gmail.com>
2025-10-01 16:22:46 +0200

    rust binding for pure dart package, do codegen in build.rs

--- nobodywho/dart/flutter_bridge/build.rs
+++ nobodywho/dart/flutter_bridge/build.rs
@@ -1,6 +1,20 @@
+use lib_flutter_rust_bridge_codegen::codegen;
+
 fn main() {
+    // do a special little dance for the androids
     let target = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     if target.contains("android") {
         println!("cargo:rustc-link-lib=c++_shared");
     }
+
+    // generate bot hrust and dart interop code
+    let config = codegen::Config {
+        rust_input: Some("crate::api".to_string()),
+        rust_root: Some(".".to_string()),
+        rust_preamble: Some("use flutter_rust_bridge::Rust2DartSendError;".to_string()),
+        dart_output: Some("../nobodywho_dart/lib/src/rust".to_string()),
+        ..Default::default()
+    };
+
+    codegen::generate(config, codegen::MetaConfig::default()).expect("Failed generating dart code");
 }
