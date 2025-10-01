814752b2d8f4d7f1c0950f51ff4c59a4a8d7a62a
AsbjornOlling <asbjornolling@gmail.com>
2025-10-01 16:21:47 +0200

    pure dart library

---
+++ nobodywho/dart/nobodywho_dart/example/nobodywho_dart_example.dart
@@ -0,0 +1,19 @@
+import 'package:nobodywho_dart/nobodywho_dart.dart';
+
+Future<void> main() async {
+  await RustLib.init();
+  // TODO: fix harcoded path
+  final model = NobodyWhoModel(modelPath: "/home/asbjorn/Development/am/nobodywho-rs/models/Qwen2.5-7B-Instruct-Q6_K_L.gguf", useGpu: true);
+  final chat = NobodyWhoChat(
+      model: model,
+      systemPrompt: "You are a helpful assistant.",
+      contextSize: 1024,
+      tools: [],
+  );
+
+  final responseStream = await chat.say(message: "Hello, friend-o!");
+
+  await for (final token in responseStream) {
+    print(token);
+  }
+}
