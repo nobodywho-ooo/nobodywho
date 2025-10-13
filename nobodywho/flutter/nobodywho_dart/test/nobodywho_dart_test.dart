d7ae33b4062224e0fbd3b227fb5ee9de8594b839
AsbjornOlling <asbjornolling@gmail.com>
2025-10-13 14:58:52 +0200

    more meaningful test

--- nobodywho/flutter/nobodywho_dart/test/nobodywho_dart_test.dart
+++ nobodywho/flutter/nobodywho_dart/test/nobodywho_dart_test.dart
@@ -1,16 +1,27 @@
 import 'package:nobodywho_dart/nobodywho_dart.dart';
 import 'package:test/test.dart';
+import 'dart:io';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
+    final modelPath = Platform.environment["TEST_MODEL"]!;
+    NobodyWhoModel? model;
+    NobodyWhoChat? chat;
 
-    setUp(() {
+    setUp(() async {
       // Additional setup goes here.
+      await RustLib.init();
+      model = NobodyWhoModel(modelPath: modelPath, useGpu: false);
+      chat = NobodyWhoChat(model: model!, systemPrompt: "", contextSize: 1024, tools: []);
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+    test('First Test', () async {
+      final responseStream = chat!.say(message: "What is the capital of Denmark?");
+      String response = "";
+      await for (final token in responseStream) {
+        response += token;
+      }
+      expect(response, contains("Copenhagen"));
     });
   });
 }
