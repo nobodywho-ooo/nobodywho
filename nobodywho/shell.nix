ba3dd91f8ae6a6244a177d94c9a1364f51c67175
AsbjornOlling <asbjornolling@gmail.com>
2025-10-15 13:48:12 +0200

    remove android stuff from default shell.nix

--- nobodywho/shell.nix
+++ nobodywho/shell.nix
@@ -1,6 +1,5 @@
 {
   pkgs ? import <nixpkgs> { },
-  android-nixpkgs,
   ...
 }:
 
@@ -9,31 +8,6 @@ let
   unity-editor = pkgs.writeShellScriptBin "unity-editor" ''
     ${pkgs.lib.getExe pkgs.unityhub.fhsEnv} ~/Unity/Hub/Editor/${unity_version}/Editor/Unity "$@"
   '';
-
-  # this NDK is the latest LTS at the time of writing
-  # https://developer.android.com/ndk/downloads
-  # ndk_version = "27.3.13750724";
-
-  # this NDK is the one mentioned in the godot docs:
-  # https://docs.godotengine.org/en/stable/contributing/development/compiling/compiling_for_android.html
-  ndk_version = "23.2.8568313";
-
-  ndk_version_dashed = builtins.replaceStrings [ "." ] [ "-" ] ndk_version;
-  ndk_version_for_android-nixpkgs = "ndk-${ndk_version_dashed}";
-  android-sdk = android-nixpkgs.sdk.x86_64-linux (
-    sdkPkgs: with sdkPkgs; [
-      cmdline-tools-latest
-      build-tools-34-0-0
-      platform-tools
-      platforms-android-34
-      emulator
-      # weird hack to get NDK in here
-      # see: https://github.com/tadfisher/android-nixpkgs/issues/113#issuecomment-3112489517
-      (sdkPkgs.${ndk_version_for_android-nixpkgs}.overrideAttrs (old: {
-        autoPatchelfIgnoreMissingDeps = true;
-      }))
-    ]
-  );
 in
 pkgs.mkShell {
   env.LIBCLANG_PATH = "${pkgs.libclang.lib}/lib/libclang.so";
@@ -99,14 +73,5 @@ pkgs.mkShell {
   ];
   shellHook = ''
     ulimit -n 2048
-
-    # https://godot-rust.github.io/book/toolchain/export-android.html
-    # export ANDROID_NDK="$ANDROID_SDK_ROOT/ndk/${ndk_version}"
-    # export CARGO_TARGET_AARCH64_LINUX_ANDROID_LINKER="$ANDROID_SDK_ROOT/ndk/${ndk_version}/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android31-clang"
-
-    echo "Environment is set up for android compilation with NDK version ${ndk_version}"
-    echo "These paths should be set in Godot Editor settings, for android export:"
-    echo "Android SDK Path: " "$ANDROID_SDK_ROOT"
-    echo "Java SDK Path: " "TBD"
   '';
 }
