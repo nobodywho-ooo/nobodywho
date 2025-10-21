735c6e9f66ece5929b54f94df43ad6779d57f0dc
Hans <hhhermansen@gmail.com>
2025-10-21 16:06:31 +0200

    Updated windows install steps. Added small model test.

---
+++ nobodywho/python/small_model_test.py
@@ -0,0 +1,18 @@
+import nobodywhopython
+import asyncio
+import sys
+
+
+model = nobodywhopython.NobodyWhoModel(sys.argv[1])
+chat = nobodywhopython.NobodyWhoChat(model, system_prompt = "You are a helpful assistant")
+
+
+
+async def main():
+
+    result = await chat.say_complete_async("What is the capital of Denmark?")
+    assert "copenhagen" in result.lower(), "Model does not know the capital of Denmark."
+        
+
+
+asyncio.run(main())
