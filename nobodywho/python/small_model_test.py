42ae8d198c3c47957486b6dc431f6860c7a24ed3
Hans <hhhermansen@gmail.com>
2025-10-21 16:40:23 +0200

    Added model test to windows.

--- nobodywho/python/small_model_test.py
+++ nobodywho/python/small_model_test.py
@@ -11,6 +11,7 @@ chat = nobodywhopython.NobodyWhoChat(model, system_prompt = "You are a helpful a
 async def main():
 
     result = await chat.say_complete_async("What is the capital of Denmark?")
+    print(result)
     assert "copenhagen" in result.lower(), "Model does not know the capital of Denmark."
         
 
