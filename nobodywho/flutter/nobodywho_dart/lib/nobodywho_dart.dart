e765dd6793e7ad073fbf7cb596895a3109d9d712
AsbjornOlling <asbjornolling@gmail.com>
2025-11-17 15:37:59 +0100

    better dart test

--- nobodywho/flutter/nobodywho_dart/lib/nobodywho_dart.dart
+++ nobodywho/flutter/nobodywho_dart/lib/nobodywho_dart.dart
@@ -1,4 +1,38 @@
 library;
 
-export 'src/rust/frb_generated.dart';
 export 'src/rust/api/nobodywho.dart';
+export 'src/rust/frb_generated.dart' show RustLib;
+
+import 'dart:async';
+import 'dart:convert';
+import 'src/rust/api/nobodywho.dart';
+
+NobodyWhoTool describeTool({
+  required Function function,
+  required String name,
+  required String description
+}) {
+  // narrow wrapper needs to be written in dart to access `function.runtimeType`
+  // and to deal with dynamic function parameters
+
+  // make it a String -> String function
+  final wrappedfunction = (String jsonString) {
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+    Map<Symbol, dynamic> namedParams = Map.fromEntries(
+      jsonMap.entries.map((e) => MapEntry(Symbol(e.key), e.value))
+    );
+    
+    final result = Function.apply(function, [], namedParams).toString();
+
+    // TODO: await
+    return result;
+  };
+
+  return newToolImpl(
+    function: wrappedfunction,
+    name: name,
+    description: description,
+    runtimeType: function.runtimeType.toString()
+  );
+}
+
--- nobodywho/flutter/nobodywho_dart/test/nobodywho_dart_test.dart
+++ nobodywho/flutter/nobodywho_dart/test/nobodywho_dart_test.dart
@@ -2,20 +2,38 @@ import 'package:nobodywho_dart/nobodywho_dart.dart';
 import 'package:test/test.dart';
 import 'dart:io';
 
+String capitalize({required String text}) {
+  return text.toUpperCase();
+}
+
 void main() {
   group('A group of tests', () {
     final modelPath = Platform.environment["TEST_MODEL"]!;
     NobodyWhoModel? model;
     NobodyWhoChat? chat;
+    NobodyWhoTool? tool;
+
+    setUpAll(() async {
+      await RustLib.init();
+    });
 
     setUp(() async {
       // Additional setup goes here.
-      await RustLib.init();
+      tool = describeTool(function: capitalize, name: "capitalize", description: "Takes a string and returns the same string capitalized.");
       model = NobodyWhoModel(modelPath: modelPath, useGpu: false);
-      chat = NobodyWhoChat(model: model!, systemPrompt: "", contextSize: 1024, tools: []);
+      chat = NobodyWhoChat(model: model!, systemPrompt: "", contextSize: 1024, tools: [tool!]);
     });
 
-    test('First Test', () async {
+    test('Capital of Denmark test', () async {
+      final responseStream = chat!.say(message: "What is the capital of Denmark?");
+      String response = "";
+      await for (final token in responseStream) {
+        response += token;
+      }
+      expect(response, contains("Copenhagen"));
+    });
+
+    test('Tool calling test', () async {
       final responseStream = chat!.say(message: "What is the capital of Denmark?");
       String response = "";
       await for (final token in responseStream) {
