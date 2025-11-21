3f10ebb90c4b0f14da559e87e34bb7d8c2951e3f
AsbjornOlling <asbjornolling@gmail.com>
2025-11-21 18:29:55 +0100

    Add binary substituters to flake config

--- flake.nix
+++ flake.nix
@@ -1,4 +1,9 @@
 {
+  nixConfig = {
+    extra-substituters = "https://cache.nixos.org https://hydra.nixos.org https://nobodywho.cachix.org https://nix-community.cachix.org";
+    extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs= nobodywho.cachix.org-1:VdcBFxLwfO1L23J973e4UolSnt3QlSZvT1E23+L+9WU= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    extra-experimental-features = "nix-command flakes";
+  };
   description = "NobodyWho - a godot plugin for NPC dialogue with local LLMs";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
