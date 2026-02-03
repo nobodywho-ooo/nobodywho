3d0b3f7789702fcb529b48b00adab6d7a43db85e
Hans <hhhermansen@gmail.com>
2026-02-03 16:54:54 +0100

    Python parsing WIP. Currently using a fork of gbnf that creates correct array rules. Still includes print statements for testing

--- nobodywho/python/src/parse.rs
+++ nobodywho/python/src/parse.rs
@@ -1,9 +1,10 @@
 // Parser module using nom
 use nom::{
     branch::alt,
-    bytes::streaming::tag,
+    bytes::{complete::tag, complete::tag_no_case},
     character::streaming::multispace0,
     combinator::map,
+    multi::separated_list1,
     sequence::{delimited, separated_pair},
     IResult,
 };
@@ -14,28 +15,56 @@ fn comma_sep(input: &str) -> IResult<&str, &str> {
 
 pub fn simple_type(input: &str) -> IResult<&str, serde_json::Value> {
     alt((
-        map(tag("str"), |_s| serde_json::json!({"type" : "string"})),
-        map(tag("int"), |_s| serde_json::json!({"type" : "integer"})),
-        map(tag("float"), |_s| serde_json::json!({"type" : "number"})),
-        map(tag("bool"), |_s| serde_json::json!({"type" : "boolean"})),
+        map(
+            tag_no_case("str"),
+            |_s| serde_json::json!({"type" : "string"}),
+        ),
+        map(
+            tag_no_case("int"),
+            |_s| serde_json::json!({"type" : "integer"}),
+        ),
+        map(
+            tag_no_case("float"),
+            |_s| serde_json::json!({"type" : "number"}),
+        ),
+        map(
+            tag_no_case("bool"),
+            |_s| serde_json::json!({"type" : "boolean"}),
+        ),
     ))(input)
+}
+
+pub fn tuple_type(input: &str) -> IResult<&str, serde_json::Value> {
+    map(
+        delimited(
+            tag_no_case("tuple["),
+            separated_list1(comma_sep, type_parser),
+            tag("]"),
+        ),
+        |vec_of_values| serde_json::json!({"type" : "array", "prefixItems" : vec_of_values, "items" : "false"}),
+    )(input)
 }
 
 pub fn list_type(input: &str) -> IResult<&str, serde_json::Value> {
     map(
-        delimited(tag("list["), type_parser, tag("]")),
+        delimited(tag_no_case("list["), type_parser, tag("]")),
         |inner| serde_json::json!({"type" : "array" , "items" : inner}),
     )(input)
 }
 
+/// This function parses types that can be dict keys. Currently this is only strings
+/// but we might want to add more types later.
 pub fn dict_keys(input: &str) -> IResult<&str, serde_json::Value> {
-    map(tag("str"), |_s| serde_json::json!({"type" : "string"}))(input)
+    map(
+        tag_no_case("str"),
+        |_s| serde_json::json!({"type" : "string"}),
+    )(input)
 }
 
 pub fn dict_type(input: &str) -> IResult<&str, serde_json::Value> {
     map(
         delimited(
-            tag("dict["),
+            tag_no_case("dict["),
             separated_pair(dict_keys, comma_sep, type_parser),
             tag("]"),
         ),
@@ -44,5 +73,5 @@ pub fn dict_type(input: &str) -> IResult<&str, serde_json::Value> {
 }
 
 pub fn type_parser(input: &str) -> IResult<&str, serde_json::Value> {
-    alt((list_type, dict_type, simple_type))(input)
+    alt((list_type, dict_type, tuple_type, simple_type))(input)
 }
