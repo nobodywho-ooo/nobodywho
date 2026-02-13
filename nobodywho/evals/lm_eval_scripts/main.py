fe66f530ee478dbd6a52b23bea9bb25ee24c04e2
AsbjornOlling <asbjornolling@gmail.com>
2026-02-13 13:33:28 +0100

    add token throughput stats

--- nobodywho/evals/lm_eval_scripts/main.py
+++ nobodywho/evals/lm_eval_scripts/main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 from pathlib import Path
 
 import lm_eval
@@ -14,15 +15,19 @@ from tqdm import tqdm
 logger = logging.getLogger(__name__)
 
 
-@register_model("nobodywho-test")
+@register_model("nobodywho")
 class NobodyWhoLM(LM):
     chat: nobodywho.Chat
+    timing_data: list[dict]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, model_path: str, allow_thinking: str, *args, **kwargs):
         super().__init__()
-        model_path = os.getenv("TEST_MODEL")
         assert isinstance(model_path, str)
-        self.chat = nobodywho.Chat(model_path, allow_thinking=True, n_ctx=16384 * 2)
+        assert Path(model_path).exists()
+        allow_thinking_bool: bool = True if allow_thinking.lower() == "true" else False
+        self.chat = nobodywho.Chat(
+            model_path, allow_thinking=allow_thinking_bool, n_ctx=16384 * 2
+        )
 
     def generate_until(self, requests: list[Instance], disable_tqdm=False):
         result: list[str | None] = []
@@ -32,15 +37,27 @@ class NobodyWhoLM(LM):
             assert isinstance(text, str)
 
             # these provide additional generation args like stopwords or max_tokens
-            request_args = request[1]
-            allow_thinking = request_args.get("allow_thinking", "true")
-            assert isinstance(allow_thinking, str)
-            allow_thinking = True if allow_thinking.lower() == "true" else False
-            self.chat.set_allow_thinking(allow_thinking)
+            # request_args = request[1]
 
             # do the generation
             try:
-                response_text = self.chat.ask(text).completed()
+                # kick of generation
+                response_stream = self.chat.ask(text)
+
+                # measure time as we go through
+                total_tokens = 0
+                start_time = time.time()
+                for _ in response_stream:
+                    total_tokens += 1
+                stop_time = time.time()
+                elapsed_time = stop_time - start_time
+
+                self.timing_data.append(
+                    {"num_tokens": total_tokens, "elapsed_time": elapsed_time}
+                )
+
+                response_text: str = response_stream.completed()
+
             except RuntimeError as e:
                 logger.error(f"Exception during generation: {e}")
                 result.append(None)
@@ -54,6 +71,15 @@ class NobodyWhoLM(LM):
 
             result.append(response_text)
         return result
+
+    def get_model_info(self):
+        """We're using this method to add additional metrics
+        This is contingent on get_model_info being called *after* running the evals
+        """
+        total_tokens = sum(d["num_tokens"] for d in self.timing_data)
+        total_time = sum(d["elapsed_time"] for d in self.timing_data)
+        avg_tokens_per_second = total_tokens / total_time
+        return {"avg_tokens_per_second": avg_tokens_per_second}
 
     def loglikelihood(self, *args, **kwargs):
         raise NotImplementedError
@@ -153,12 +179,12 @@ if __name__ == "__main__":
 
     print("Starting evals suite...")
     results = lm_eval.simple_evaluate(
-        model="nobodywho-test",
-        model_args={"allow_thinking": "True"},
+        model="nobodywho",
+        model_args={"model_path": str(model_path.resolve()), "allow_thinking": "true"},
         tasks=tasks,
         log_samples=True,
         evaluation_tracker=tracker,
-        limit=10,
+        limit=5,
     )
     assert results is not None
 
