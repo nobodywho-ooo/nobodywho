bf27202f80841daa0935c90eed1955d6ec74b27f
Hans <hhhermansen@gmail.com>
2026-06-17 17:08:04 +0200

    Update gemma4 to download from our huggingfrace

--- nobodywho/models.nix
+++ nobodywho/models.nix
@@ -16,13 +16,13 @@
     sha256 = "sha256-pDx8mxGkwVF+W/lRUZYOFiHRty96STNksB44bPGqodM=";
   };
   TEST_MULTIMODAL_MODEL = fetchurl {
-    name = "google_gemma-4-E2B-it-Q4_K_M.gguf";
-    url = "https://huggingface.co/bartowski/google_gemma-4-E2B-it-GGUF/resolve/main/google_gemma-4-E2B-it-Q4_K_M.gguf";
-    sha256 = "sha256-tTEDQLOiPTFlXXEZ0QDV3xstjuF7PKiwojrX6etfpwU=";
+    name = "gemma-4-E2B-it-Q4_K_M.gguf";
+    url = "https://huggingface.co/NobodyWho/Google_Gemma4-E2B-GGUF/resolve/main/gemma-4-E2B-it-Q4_K_M.gguf";
+    sha256 = "sha256-k3i8RxcQIp7xZXCbYuNL+2IjFCDdr21ynnJzBbW4Zy0=";
   };
   TEST_MMPROJ_MODEL = fetchurl {
-    name = "mmproj-google_gemma-4-E2B-it-f16.gguf";
-    url = "https://huggingface.co/bartowski/google_gemma-4-E2B-it-GGUF/resolve/main/mmproj-google_gemma-4-E2B-it-f16.gguf";
-    sha256 = "sha256-M5fy4VhZ0mjckXgukucMSFx3gESNV9Yi0UU3Q5AWj3E=";
+    name = "mmproj-BF16.gguf";
+    url = "https://huggingface.co/NobodyWho/Google_Gemma4-E2B-GGUF/resolve/main/mmproj-BF16.gguf";
+    sha256 = "sha256-pALxD7V4C/kdA6EM2JBhE59SK+4uZ5sSkbv9zXHZVH0=";
   };
 }
