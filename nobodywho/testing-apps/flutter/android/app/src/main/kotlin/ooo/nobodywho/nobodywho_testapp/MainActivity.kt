68de7b39f2b9d659db932b4c0393960cfc7155c5
Martin Krebs <krebsmato@gmail.com>
2026-09-21 16:19:42 +0200

    feat: disable fp16 on ardeno

--- nobodywho/testing-apps/flutter/android/app/src/main/kotlin/ooo/nobodywho/nobodywho_testapp/MainActivity.kt
+++ nobodywho/testing-apps/flutter/android/app/src/main/kotlin/ooo/nobodywho/nobodywho_testapp/MainActivity.kt
@@ -1,5 +1,12 @@
 package ooo.nobodywho.nobodywho_testapp
 
+import android.os.Bundle
+import android.system.Os
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Os.setenv("GGML_OPENCL_ADRENO_XMEM_GEMM", "0", true)
+        super.onCreate(savedInstanceState)
+    }
+}
