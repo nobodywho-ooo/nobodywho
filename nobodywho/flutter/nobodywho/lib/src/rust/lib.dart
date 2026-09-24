e33fd9b7c0cda7f685486dff662cd43b8e49736d
Martin Krebs <krebsmato@gmail.com>
2026-09-24 15:02:37 +0200

    fix: revert

--- nobodywho/flutter/nobodywho/lib/src/rust/lib.dart
+++ nobodywho/flutter/nobodywho/lib/src/rust/lib.dart
@@ -4,7 +4,9 @@
 // ignore_for_file: invalid_use_of_internal_member, unused_import, unnecessary_import
 
 import 'dart:convert';
+
 import 'frb_generated.dart';
+
 import 'package:flutter_rust_bridge/flutter_rust_bridge_for_generated.dart';
 import 'package:freezed_annotation/freezed_annotation.dart' hide protected;
 part 'lib.freezed.dart';
